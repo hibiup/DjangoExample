ec4abe7bc531c3fdd3c836d5b53667ef2a2175ec
Jeff <jeffwji@gmail.com>
2019-07-21 12:24:19 -0400

    Creation

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name=' DjangoExample',
+    author='Jeff',
+
+    version_command='git describe --always --long --dirty=-dev',
+    packages=find_packages(
+        exclude=['tests', '*.tests', '*.tests.*']
+    ),
+    package_data={
+        '': ['config/*.properties', '*.md', 'requirements.txt'],
+    },
+    install_requires=requirements,
+)
